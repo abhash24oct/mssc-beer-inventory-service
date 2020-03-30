76bf79816eb23861a92f7770e3ef59617b203547
Abhash <abhash24oct@gmail.com>
2020-03-30 19:05:29 +0200

    Corrected the test

--- src/test/java/abhash/springframewrok/msscbeerinventoryservice/web/controller/BeerInventoryControllerTest.java
+++ src/test/java/abhash/springframewrok/msscbeerinventoryservice/web/controller/BeerInventoryControllerTest.java
@@ -1,15 +1,16 @@
 package abhash.springframewrok.msscbeerinventoryservice.web.controller;
 
 import abhash.springframewrok.msscbeerinventoryservice.bootstrap.BeerInventoryBootstrap;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import abhash.springframewrok.msscbeerinventoryservice.repository.BeerInventoryRepository;
+import abhash.springframewrok.msscbeerinventoryservice.web.mappers.BeerInventoryMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import static  org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BeerInventoryController.class)
@@ -18,15 +19,17 @@ class BeerInventoryControllerTest {
     @Autowired
     MockMvc mockMvc;
 
-    @Autowired
-    ObjectMapper objectMapper;
+    @MockBean
+    BeerInventoryMapper beerInventoryMapper;
 
+    @MockBean
+    BeerInventoryRepository beerInventoryRepository;
 
 
     @Test
     void listBeersById() throws Exception {
 
-        mockMvc.perform(get("/api/v1/beer/"+ BeerInventoryBootstrap.BEER_1_UPC+"/inventory").accept(MediaType.APPLICATION_JSON))
+        mockMvc.perform(get("/api/v1/beer/"+ BeerInventoryBootstrap.BEER_2_UUID+"/inventory").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
 }
